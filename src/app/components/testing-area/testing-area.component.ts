4d99dfd463b30325e62a9a1f63da4e93a7810ee9
Om Komawar <omkomawar222@gmail.com>
2021-06-13 16:54:25 +0530

    created testing area component

---
+++ src/app/components/testing-area/testing-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-area',
+  templateUrl: './testing-area.component.html',
+  styleUrls: ['./testing-area.component.scss']
+})
+export class TestingAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
