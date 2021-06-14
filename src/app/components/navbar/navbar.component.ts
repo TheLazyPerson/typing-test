db3441d71f9fb30a9c14d04f4be8efccad720469
Om Komawar <omkomawar222@gmail.com>
2021-06-14 13:53:10 +0530

    changed navbar view if the test is started

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Test } from 'src/app/shared/models/test.model';
+import { TestService } from 'src/app/shared/services/test.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  test: Test;
+  constructor(testService: TestService) {
+    testService.currentTest.subscribe(test => this.test = test);
+  }
 
   ngOnInit(): void {
   }
