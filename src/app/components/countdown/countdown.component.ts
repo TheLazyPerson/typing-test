f58ac8748575af7f112a8eb0b0e7b08aba02b0fc
Om Komawar <omkomawar222@gmail.com>
2021-06-14 13:50:41 +0530

    added countdown to selected time and emitted complete event after countdown is completed

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { Test } from 'src/app/shared/models/test.model';
+import { Component,  OnInit, Output, EventEmitter } from '@angular/core';
+import { interval,  Observable } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 import { TestService } from 'src/app/shared/services/test.service';
+
 
 @Component({
   selector: 'app-countdown',
@@ -12,12 +14,26 @@ export class CountdownComponent implements OnInit {
   seconds: number = 0;
   @Output() complete: EventEmitter<string> = new EventEmitter<string>()
 
-  constructor(testService: TestService) {
+  constructor(private testService: TestService) {
     testService.currentTest.subscribe(test => this.seconds = test.time);
 
   }
 
   ngOnInit(): void {
+    var countdown: Observable<number> = interval(1000);
+    countdown.pipe(takeWhile(() => this.seconds > 0)).subscribe(() => {
+      this.seconds = this.seconds - 1;
+      this.testService.setTime(this.seconds);
+
+    },
+      (err) => {
+        console.log("ERROR", err);
+      },
+      () => {
+        this.complete.emit("complete");
+        console.log("TEST COMPLETE");
+    });
+
   }
 
 }
